e4fbabe7ac26a371d1fa5a496d320ebb49ba0faa
Matt Jibson <matt.jibson@gmail.com>
2014-01-12 04:02:59 -0500

    Stream data instead of generating all upfront
    This prevents the PA buffer from filling up (I think).

--- codec/nsf/nsf_test.go
+++ codec/nsf/nsf_test.go
@@ -1,7 +1,6 @@
 package nsf
 
 import (
-	"fmt"
 	"os"
 	"testing"
 	"time"
@@ -21,36 +20,32 @@ func TestNSF(t *testing.T) {
 	if n.LoadAddr != 0x8000 || n.InitAddr != 0x8003 || n.PlayAddr != 0x8000 {
 		t.Error("bad addresses")
 	}
+
+	pa := pulsego.NewPulseMainLoop()
+	defer pa.Dispose()
+	pa.Start()
+
+	ctx := pa.NewContext("default", 0)
+	if ctx == nil {
+		t.Fatal("Failed to create a new context")
+	}
+	defer ctx.Dispose()
+	st := ctx.NewStream("default", &pulsego.PulseSampleSpec{
+		Format: pulsego.SAMPLE_FLOAT32LE, Rate: SampleRate, Channels: 1})
+	if st == nil {
+		t.Fatal("Failed to create a new stream")
+	}
+	defer st.Dispose()
+	st.ConnectToSink()
+
 	n.Init(1)
-
-	d := time.Second * 10
-	samples := n.Play(d)
-	fmt.Println("samples", len(samples))
-	ns := 0
-	for _, s := range samples {
-		if s != 0 {
-			ns++
-		}
-	}
-	fmt.Println("ns", ns)
-	if ns > 0 {
-		pa := pulsego.NewPulseMainLoop()
-		defer pa.Dispose()
-		pa.Start()
-
-		ctx := pa.NewContext("default", 0)
-		if ctx == nil {
-			t.Fatal("Failed to create a new context")
-		}
-		defer ctx.Dispose()
-		st := ctx.NewStream("default", &pulsego.PulseSampleSpec{
-			Format: pulsego.SAMPLE_FLOAT32LE, Rate: SampleRate, Channels: 1})
-		if st == nil {
-			t.Fatal("Failed to create a new stream")
-		}
-		defer st.Dispose()
-		st.ConnectToSink()
+	d := time.Second / 5
+	for {
+		wait := time.After(d)
+		samples := n.Play(d)
+		println("samples", len(samples))
 		st.Write(samples, pulsego.SEEK_RELATIVE)
-		time.Sleep(d)
+		<-wait
 	}
+	time.Sleep(d)
 }
