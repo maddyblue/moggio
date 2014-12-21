07fef6fe557d23c14e560101d02ea9f06256f9a0
Matt Jibson <matt.jibson@gmail.com>
2014-12-20 19:15:03 -0500

    Run go generate manually when desired

--- w.sh
+++ w.sh
@@ -2,15 +2,9 @@
 
 set -e
 
-escape () {
-	go generate
-}
-
 while true; do
-	escape
 	go run main.go -w -dropbox rnhpqsbed2q2ezn:ldref688unj74ld -drive 256229448371-93bchgphf79q2vbik5aod4osvksce35p.apps.googleusercontent.com:zO5-2BqMb5Zl4EFKd3fVnavw -soundcloud ec28c2226a0838d01edc6ed0014e462e:a115e94029d698f541960c8dc8560978
 	if [ $? != 0 ] ; then
-		escape
 		exit
 	fi
 	echo restarting
