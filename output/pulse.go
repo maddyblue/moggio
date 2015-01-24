f0f5bc641e1bfc5607e9fbf7a9aefa12ff5ea17e
Matt Jibson <matt.jibson@gmail.com>
2015-01-24 03:48:00 -0500

    New output API support for pulseaudio

--- output/pulse.go
+++ output/pulse.go
@@ -10,42 +10,34 @@ import (
 	"github.com/mesilliac/pulse-simple"
 )
 
-var ports = make(map[config]*port)
-
-type config struct {
-	sr, ch int
-}
-
-type port struct {
+type output struct {
 	st *pulse.Stream
 }
 
-func Get(sampleRate, channels int) (Output, error) {
-	c := config{
-		sr: sampleRate,
-		ch: channels,
-	}
-	o := ports[c]
-	if o == nil {
-		o = new(port)
-		var err error
-		ss := pulse.SampleSpec{pulse.SAMPLE_FLOAT32LE, uint32(sampleRate), uint8(channels)}
-		o.st, err = pulse.Playback("mog", "mog", &ss)
-		if err != nil {
-			return nil, err
-		}
-		ports[c] = o
+func get(sampleRate, channels int) (Output, error) {
+	o := new(output)
+	var err error
+	ss := pulse.SampleSpec{pulse.SAMPLE_FLOAT32LE, uint32(sampleRate), uint8(channels)}
+	o.st, err = pulse.Playback("mog", "mog", &ss)
+	if err != nil {
+		return nil, err
 	}
 	return o, nil
 }
 
-func (p *port) Push(samples []float32) {
+func (o *output) Push(samples []float32) {
 	buf := new(bytes.Buffer)
 	for _, s := range samples {
 		_ = binary.Write(buf, binary.LittleEndian, s)
 	}
-	_, err := p.st.Write(buf.Bytes())
+	_, err := o.st.Write(buf.Bytes())
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func (o *output) Start() {
+}
+
+func (o *output) Stop() {
 }
