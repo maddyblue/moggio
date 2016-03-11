38ddfcfe2e8a1979b4a9c9b7c87419ffadf271ef
Matt Jibson <matt.jibson@gmail.com>
2016-03-10 21:12:37 -0500

    Use modd to watch changes

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"flag"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"runtime"
 	"strings"
@@ -14,7 +12,6 @@ import (
 
 	"github.com/facebookgo/httpcontrol"
 	"github.com/mjibson/moggio/server"
-	"gopkg.in/fsnotify.v1"
 
 	// codecs
 	_ "github.com/mjibson/moggio/codec/flac"
@@ -37,7 +34,6 @@ import (
 
 var (
 	flagAddr       = flag.String("addr", ":6601", "listen address")
-	flagWatch      = flag.Bool("w", false, "watch current directory and exit on changes; for use with an autorestarter")
 	flagDrive      = flag.String("drive", "792434736327-0pup5skbua0gbfld4min3nfv2reairte.apps.googleusercontent.com:OsN_bydWG45resaU0PPiDmtK", "Google Drive API credentials of the form ClientID:ClientSecret")
 	flagDropbox    = flag.String("dropbox", "rnhpqsbed2q2ezn:ldref688unj74ld", "Dropbox API credentials of the form ClientID:ClientSecret")
 	flagSoundcloud = flag.String("soundcloud", "ec28c2226a0838d01edc6ed0014e462e:a115e94029d698f541960c8dc8560978", "SoundCloud API credentials of the form ClientID:ClientSecret")
@@ -54,10 +50,6 @@ func main() {
 			MaxTries:              3,
 			RetryAfterTimeout:     true,
 		},
-	}
-	if *flagWatch {
-		watch(".", "*.go", quit)
-		go browserify()
 	}
 	redir := *flagAddr
 	if strings.HasPrefix(redir, ":") {
@@ -100,92 +92,6 @@ func main() {
 		}
 	}
 	log.Fatal(server.ListenAndServe(*stateFile, *flagAddr, "", *flagDev))
-}
-
-func quit() {
-	os.Exit(0)
-}
-
-func browserify() {
-	base := filepath.Join("server", "static")
-	src := filepath.Join(base, "src")
-	js := filepath.Join(base, "js")
-	log.Println("starting watchify")
-	c := exec.Command("watchify",
-		"-t", "[", "reactify", "--es6", "]",
-		filepath.Join(src, "nav.js"),
-		"-o", filepath.Join(js, "moggio.js"),
-		"--verbose",
-	)
-	c.Stderr = os.Stderr
-	c.Stdout = os.Stdout
-	if err := c.Start(); err != nil {
-		log.Fatal(err)
-	}
-	if err := c.Wait(); err != nil {
-		log.Printf("browserify error: %v", err)
-	}
-}
-
-func run(name string, arg ...string) func() {
-	return func() {
-		log.Println("running", name)
-		c := exec.Command(name, arg...)
-		stdout, err := c.StdoutPipe()
-		if err != nil {
-			log.Fatal(err)
-		}
-		stderr, err := c.StderrPipe()
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := c.Start(); err != nil {
-			log.Fatal(err)
-		}
-		go func() { io.Copy(os.Stdout, stdout) }()
-		go func() { io.Copy(os.Stderr, stderr) }()
-		if err := c.Wait(); err != nil {
-			log.Printf("run error: %v: %v", name, err)
-		}
-		log.Println("run complete:", name)
-	}
-}
-
-func watch(root, pattern string, f func()) {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if matched, err := filepath.Match(pattern, info.Name()); err != nil {
-			log.Fatal(err)
-		} else if !matched {
-			return nil
-		}
-		err = watcher.Add(path)
-		if err != nil {
-			log.Fatal(err)
-		}
-		return nil
-	})
-	log.Println("watching", pattern, "in", root)
-	wait := time.Now()
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				if wait.After(time.Now()) {
-					continue
-				}
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					f()
-					wait = time.Now().Add(time.Second * 2)
-				}
-			case err := <-watcher.Errors:
-				log.Println("error:", err)
-			}
-		}
-	}()
 }
 
 //go:generate browserify -t [ reactify --es6 ] server/static/src/nav.js -o server/static/js/moggio.js
