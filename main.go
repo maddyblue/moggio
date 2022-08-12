e73d8bd7b7d030d763591c16a1ec4a38024687c7
Matt Jibson <matt.jibson@gmail.com>
2022-08-11 22:15:57 -0600

    remove unused protocols

--- main.go
+++ main.go
@@ -26,18 +26,13 @@ import (
 	// _ "github.com/mjibson/moggio/protocol/bandcamp"
 
 	_ "github.com/mjibson/moggio/protocol/file"
-	_ "github.com/mjibson/moggio/protocol/gmusic"
-	"github.com/mjibson/moggio/protocol/soundcloud"
 	_ "github.com/mjibson/moggio/protocol/stream"
 )
 
 var (
-	flagAddr       = flag.String("addr", ":6601", "listen address")
-	flagDrive      = flag.String("drive", "792434736327-0pup5skbua0gbfld4min3nfv2reairte.apps.googleusercontent.com:OsN_bydWG45resaU0PPiDmtK", "Google Drive API credentials of the form ClientID:ClientSecret")
-	flagDropbox    = flag.String("dropbox", "rnhpqsbed2q2ezn:ldref688unj74ld", "Dropbox API credentials of the form ClientID:ClientSecret")
-	flagSoundcloud = flag.String("soundcloud", "ec28c2226a0838d01edc6ed0014e462e:a115e94029d698f541960c8dc8560978", "SoundCloud API credentials of the form ClientID:ClientSecret")
-	flagDev        = flag.Bool("dev", false, "enable dev mode")
-	stateFile      = flag.String("state", "", "specify non-default statefile location")
+	flagAddr  = flag.String("addr", ":6601", "listen address")
+	flagDev   = flag.Bool("dev", false, "enable dev mode")
+	stateFile = flag.String("state", "", "specify non-default statefile location")
 )
 
 func main() {
@@ -54,27 +49,6 @@ func main() {
 		redir = "localhost" + redir
 	}
 	redir = "http://" + redir + "/api/oauth/"
-	if *flagDrive != "" {
-		sp := strings.Split(*flagDrive, ":")
-		if len(sp) != 2 {
-			log.Fatal("bad drive string %s", *flagDrive)
-		}
-		//drive.Init(sp[0], sp[1], redir)
-	}
-	if *flagDropbox != "" {
-		sp := strings.Split(*flagDropbox, ":")
-		if len(sp) != 2 {
-			log.Fatal("bad drive string %s", *flagDropbox)
-		}
-		//dropbox.Init(sp[0], sp[1], redir)
-	}
-	if *flagSoundcloud != "" {
-		sp := strings.Split(*flagSoundcloud, ":")
-		if len(sp) != 2 {
-			log.Fatal("bad drive string %s", *flagSoundcloud)
-		}
-		soundcloud.Init(sp[0], sp[1], redir)
-	}
 	if *stateFile == "" {
 		switch {
 		case *flagDev:
